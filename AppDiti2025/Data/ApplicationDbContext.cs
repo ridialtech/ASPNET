11c7e5a82923d4725c5300c980bee06a92417ee8
ridialtech <103546771+ridialtech@users.noreply.github.com>
2025-07-07 17:25:57 +0000

    Configure static files and ensure DB creation

--- AppDiti2025/Data/ApplicationDbContext.cs
+++ AppDiti2025/Data/ApplicationDbContext.cs
@@ -3,10 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AppDiti2025.Data;
 
+using AppDiti2025.Models;
+
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<Admin> Admins { get; set; } = default!;
+    public DbSet<Client> Clients { get; set; } = default!;
 }
