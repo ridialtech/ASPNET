4de9819812258a56ad3eb7553c3878a1acb59d40
ridialtech <103546771+ridialtech@users.noreply.github.com>
2025-07-07 18:16:57 +0000

    Remove Admin and Client features

--- AppDiti2025/Data/ApplicationDbContext.cs
+++ AppDiti2025/Data/ApplicationDbContext.cs
@@ -3,10 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AppDiti2025.Data;
 
+using AppDiti2025.Models;
+
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+
+    // Additional entity sets will be added here
 }
