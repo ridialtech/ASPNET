50ca59f7f605ba083ec6ab6b055821732240693a
ridialtech <103546771+ridialtech@users.noreply.github.com>
2025-07-07 17:22:30 +0000

    Fix newline and BOM

--- AppDiti2025/Data/ApplicationDbContext.cs
+++ AppDiti2025/Data/ApplicationDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AppDiti2025.Data;
+
+using AppDiti2025.Models;
 
 public class ApplicationDbContext : IdentityDbContext
 {
@@ -9,4 +11,7 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Admin> Admins { get; set; } = default!;
+    public DbSet<Client> Clients { get; set; } = default!;
 }
